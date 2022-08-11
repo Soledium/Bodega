4971bcf9ea284dda99cd64798531229ac55f4ba9
Soledium <nescobar3@hotmail.com>
2022-08-11 09:21:28 -0400

    Avances ejercicio extra

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  get 'dashboard/index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  root "dashboard#index"
+  root "dashboard#index"  
+  get 'home/index'
+  devise_for :users
+    resources :products do
+    member do
+      get :new_movement
+      post :create_movement
+    end
+  end
 end
